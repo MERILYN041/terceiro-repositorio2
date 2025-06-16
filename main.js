c6fe833788fdb35a30a088fef720b27bc2545ad8
MERILYN041 <144914757+MERILYN041@users.noreply.github.com>
2025-06-16 08:50:10 -0300

    1

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const numeroSenha = document.querySelector('.parametro-senha_texto');
+const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
-const botoes =document.querySelectorAll('.parametro-senha_botao');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 console.log(botoes);
