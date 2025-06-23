33b16741ce4d8c3db2786aa8c5dc40f94e120402
MERILYN041 <144914757+MERILYN041@users.noreply.github.com>
2025-06-23 08:50:00 -0300

    4

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function geraSenha() {
     for(let i=0; i < tamanhoSenha; i++) {
     let numeroAleatorio = Math.random()*letrasMaiusculas.length;
     numeroAleatorio = Math.floor(numeroAleatorio);
-senha =senha + letrasMaiusculas[numeroAleatorio];
+senha = senha + letrasMaiusculas[numeroAleatorio];
    }
-   campoSenha.value = letrasMaiusculas;
+   campoSenha.value = senha;
 }
