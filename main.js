95e886e56f2c2d163172b5b458610708b07a61ce
MERILYN041 <144914757+MERILYN041@users.noreply.github.com>
2025-07-07 08:42:06 -0300

    10

--- main.js
+++ main.js
@@ -54,10 +54,11 @@ function geraSenha() {
         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
-    classificaSenha(); 
+    classificaSenha(alfabeto.length); 
 }
 
-function classificaSenha() {
+function classificaSenha(tamanhoAlfabeto) {
+    let entropia = tamanhoSenha*Math.log2(alfabeto.length);
     forcaSenha.classList.remove('fraca', 'media', 'forte');
     if (tamanhoSenha>11){
         forcaSenha.classList.add('forte');
